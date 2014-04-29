77e518f1e3c7744425ac38a14a91e372bb812fdc
HVboom <GitHub@HVboom.ch>
2014-04-29 23:44:58 +0200

    rails_apps_composer: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+PhotoGallery::Application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
